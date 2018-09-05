9f1f758556ca4ea2e99db16b92e3a78572f24923
Peter <p_wang@outlook.com>
2018-09-06 00:08:49 +1000

    Added About page and fixed routing.

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/login')
 def login():
