13abfb1f60ecc49127c09244901fd7094717cd5e
Qifu LU <Qifu1995@outlook.com>
2018-09-14 17:53:09 +1000

    add register html, set up user database modle,get routes work

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #encoding: utf-8
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from models import *
 import config
+from myform import *
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -11,15 +12,45 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    stickerform = StickerForm()
+    # if method =='POST' and the user's input matches validator'srequirement
+    # then return true
+    # set csrf_token : OFF in config.py
+    if stickerform.validate_on_submit():
+        this_sticker = stickerform.sticker.data
+        return redirect(url_for('stock',stockname=this_sticker))
+
+    return render_template('index.html', thisform=stickerform)
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    registerform = RegistrationForm()
+
+    if registerform.validate_on_submit():
+        this_username = registerform.username.data
+        this_email = registerform.email.data
+        this_password = registerform.password.data
+        this_user = (this_username, this_password, this_email)
+        print(this_user)
+        # db.session.add(this_user)
+        return ('register successfully')
+    return render_template('register.html',thisform=registerform)
+
+
+@app.route('/stock/<stockname>')
+def stock(stockname):
+    # Search any stock market info by this stock name
+    # then show them in the following template
+    # .....to implement.........
+    otherStickerForm = StickerForm()
+    return render_template('stock.html', stock_name=stockname, thisform=otherStickerForm)
 
 @app.route('/feature')
 def feature():
--- models.py
+++ models.py
@@ -1,16 +1,21 @@
-#encoding: utf-8
+# encoding: utf-8
 from db_sqlalchemy import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(50), nullable=False, unique=True)
+    user_pass = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    fav_stock_list = db.Column(db.String(255), nullable=True)
+    my_stocks = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, uid, email):
-        self.user_id = uid
+    def __init__(self, uname, upass, email, favstocklist=None, mystocks=None):
+        self.user_name = uname
+        self.user_pass = upass
         self.email = email
+        self.fav_stock_list = favstocklist
+        self.my_stocks = mystocks
 
     def __repr__(self):
-        return '<User %r>' % self.user_id
-
+        return '<User: %r>' % self.uid
