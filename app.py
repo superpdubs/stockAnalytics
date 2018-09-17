d0f369dbd40a7ed5ac74f03d53e60e5f01ee8421
Winson Wen <hestokaza@gmail.com>
2018-09-18 03:59:32 +1000

    Remove no-cache, reduce div use, tweak styling

--- app.py
+++ app.py
@@ -64,13 +64,5 @@ def sources():
     otherStickerForm = StickerForm()
     return render_template('sources.html', thisform=otherStickerForm)
 
-# disable caching
-@app.after_request
-def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    return r
-
 if __name__ == '__main__':
     app.run(debug=True)
