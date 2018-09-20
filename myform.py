19c31216d2834d78e2cd5ecfd0891d04932f2da9
Winson Wen <hestokaza@gmail.com>
2018-09-20 23:03:11 +1000

    Disable autocomplete

--- myform.py
+++ myform.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo,InputRequire
 
 
 class StockForm(FlaskForm):
-    stock = StringField('Stock', render_kw={"placeholder": "Search"})
+    stock = StringField('Stock', render_kw={"placeholder": "Search", "autocomplete": "off"})
     submit = SubmitField('Go Fetch !')
 
 
