7f1ea7d3e9078dd651edf34ae6c4d6d4740e7149
Patrick Ngo <PatrickNgo@users.noreply.github.com>
2018-10-09 22:34:51 +1100

    turned classifier into a function
    added global variables

--- ml/classifier.py
+++ ml/classifier.py
@@ -9,28 +9,31 @@
 ################################################################
 
 # retrieve tweet from API and put into a list
-tweets_from_API = ['@TSLA tesla is such a great stock to buy', 'today was a terrible day @GOOG sad', 'wobcke grumpy #TSLA']
-cleaned_tweets = []
-num_tweets = len(tweets_from_API)
+def classifier(tweets_from_API):
+ #tweets_from_API = ['@TSLA tesla is such a great stock to buy', 'today was a terrible day @GOOG sad', 'wobcke grumpy #TSLA']
+    cleaned_tweets = []
+    num_tweets = len(tweets_from_API)
 
-# clean tweets
-cleaned_tweets = [tweet_cleaner(tweet) for tweet in tweets_from_API]
+    # clean tweets
+    cleaned_tweets = [tweet_cleaner(tweet) for tweet in tweets_from_API]
 
-# convert to series 
-new_tweet = pd.Series(cleaned_tweets)
+    # convert to series 
+    new_tweets = pd.Series(cleaned_tweets)
 
-# feed tweets into doc2vec model
-infer_vector = [model_ug_dbow.infer_vector(tweet) for tweet in new_tweet]
-for i in range(0,num_tweets-1):
-    if i == 0:
-        input_vectors = infer_vector[i]
-    input_vectors = np.append(input_vectors, infer_vector[i])
+    # feed tweets into doc2vec model
+    infer_vector = [model_ug_dbow.infer_vector(tweet) for tweet in new_tweet]
+    for i in range(0,num_tweets-1):
+        if i == 0:
+            input_vectors = infer_vector[i]
+        input_vectors = np.append(input_vectors, infer_vector[i])
     
-# classify tweets as good/bad
-ynew = new_model.predict_classes(input_vectors.reshape(num_tweets,100))
-
-# classification vector of 0s and 1s
-ynew # I forgot which is which i.e. (0,1)=(good, bad) or vice-versa)
+    # classify tweets as good/bad
+    ynew = new_model.predict_classes(input_vectors.reshape(num_tweets,100))
+    
+    # classification vector of 0s and 1s
+    # example output: [[0],[1],[1]]
+    print(ynew) # I forgot which is which i.e. (0,1)=(good, bad) or vice-versa
+    return ynew
 
 
 
