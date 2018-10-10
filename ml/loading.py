c2fda585bfc93628a6df37e2a76bb77db3ccd0fe
Luke <lukelalalala@gmail.com>
2018-10-10 23:08:16 +1100

    embedded ML into webapp

--- ml/loading.py
+++ ml/loading.py
@@ -2,23 +2,21 @@
 
 # Script used to load all required data, functions, and models
 
+import numpy as np
 import keras
 from gensim.models import Doc2Vec
 
-def load():    
-    # doc2vec 
-    global model_ug_dbow
-    model_ug_dbow = Doc2Vec.load('d2v_model_ug_dbow.doc2vec')
+def loadDoc():
+    # doc2vec
+    model_ug_dbow = Doc2Vec.load('ml/d2v_model_ug_dbow.doc2vec')
     model_ug_dbow.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
 
     print('loaded doc2vec')
-    
+    return model_ug_dbow
+
+def loadNeural():
     # neural network
-    global neural_model
-    neural_model = keras.models.load_model('my_model.h5')
+    neural_model = keras.models.load_model('ml/my_model.h5')
+    print('testing model:', neural_model.predict(np.zeros((3,100))))
     neural_model.summary()
-
-
-
-
-
+    return neural_model
