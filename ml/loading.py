70f547765fe8c9f9e7f99a571068873e536a9882
Patrick <patrick@Patricks-MacBook-Air.local>
2018-10-10 12:16:41 +1100

    cleaned

--- ml/loading.py
+++ ml/loading.py
@@ -14,38 +14,12 @@ import multiprocessing
 from sklearn import utils
 from bs4 import BeautifulSoup
 from nltk.tokenize import WordPunctTokenizer
-    
+
+
 def load():
     ################################################################
     # DATA CLEANING AND PREPARATION
     ################################################################
-    def tweet_cleaner(text):
-        tok = WordPunctTokenizer()
-        pat1 = r'@[A-Za-z0-9_]+' # removing @ tags              
-        pat2 = r'https?://[^ ]+' # removing https pages
-        combined_pat = r'|'.join((pat1, pat2))
-        www_pat = r'www.[^ ]+'   # removing www pages
-        # handling contraction words
-        negations_dic = {"isn't":"is not", "aren't":"are not", "wasn't":"was not", "weren't":"were not",
-                    "haven't":"have not","hasn't":"has not","hadn't":"had not","won't":"will not",
-                    "wouldn't":"would not", "don't":"do not", "doesn't":"does not","didn't":"did not",
-                    "can't":"can not","couldn't":"could not","shouldn't":"should not","mightn't":"might not",
-                    "mustn't":"must not"}
-        neg_pattern = re.compile(r'\b(' + '|'.join(negations_dic.keys()) + r')\b')
-        soup = BeautifulSoup(text, 'lxml')
-        souped = soup.get_text()
-        try:
-            bom_removed = souped.decode("utf-8-sig").replace(u"\ufffd", "?")
-        except:
-            bom_removed = souped
-        stripped = re.sub(combined_pat, '', bom_removed)
-        stripped = re.sub(www_pat, '', stripped)
-        lower_case = stripped.lower()
-        neg_handled = neg_pattern.sub(lambda x: negations_dic[x.group()], lower_case)
-        letters_only = re.sub("[^a-zA-Z]", " ", neg_handled)
-        # tokenize and join to remove uncessary white spaces
-        words = [x for x  in tok.tokenize(letters_only) if len(x) > 1]
-        return (" ".join(words)).strip()
 
     csv = 'clean_tweet.csv'
     my_df = pd.read_csv(csv,index_col=0)
